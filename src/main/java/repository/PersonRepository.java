8d3f8843c7389c9753dc58bec16e0e5103746059
zhv <zhslma@gmail.com>
2017-06-17 18:09:19 +0400

    * changes after review

---
+++ src/main/java/repository/PersonRepository.java
@@ -0,0 +1,15 @@
+package repository;
+
+import model.Person;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author VYZH
+ * @since 16.06.2017
+ */
+@Repository
+public interface PersonRepository extends CrudRepository<Person, Integer> {
+
+    Person findByLastName(String lastName);
+}
